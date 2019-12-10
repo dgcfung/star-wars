3be5b280ced5aacadcf864d30ed9690a58a2535c
B Legit <fungd@Donalds-MacBook-Air.local>
2019-12-09 19:04:10 -0500

    HW Day 1

--- act2.sh
+++ act2.sh
@@ -1,17 +1,21 @@
 # 1. Inside the `star_wars/rebellion` directory, [IN ONE COMMAND!!] create a file called `princess_leia` with the text `Help me, Obi-Wan…You’re my only hope.`
-echo 'Help me, Obi-Wan…You’re my only hope.' >>  star_wars/rebellion/princess_leia
+echo 'Help me, Obi-Wan…You’re my only hope.' 
+//cd ..
+//cd rebellion
+//touch princess_leia.txt
+echo "Help me, Obi-Wan...You're my only hope">>princess_leia.txt
 # 2. Create a file called obi_wan in star_wars/rebellion.
-touch star_wars/rebellion/obi_wan
+touch obi_wan.txt
 # 3. Create a file in star_wars/rebellion called luke_skywalker.
-touch star_wars/rebellion/luke_skywalker
+touch luke_skywalker
 # 4. Create a *directory* in star_wars/rebellion called millenium_falcon.
-mkdir star_wars/rebellion/millenium_falcon
+mkdir millenium_falcon/
 # 5. In two commands, inside the millenium_falcon, create two files: han_solo and chewy.
-touch star_wars/rebellion/millenium_falcon/han_solo
-touch star_wars/rebellion/millenium_falcon/chewy
+touch han_solo.txt 
+touch chewy.txt
 # 6. In THREE commands, move luke_skywalker, obi_wan, and princess_leia into the millenium_falcon, respectively.
-mv star_wars/rebellion/luke_skywalker star_wars/rebellion/millenium_falcon
-mv star_wars/rebellion/obi_wan star_wars/rebellion/millenium_falcon
-mv star_wars/rebellion/princess_leia star_wars/rebellion/millenium_falcon
+mv luke_skywalker.txt millenium_falcon/
+mv obi_wan.txt millenium_falcon/
+mv princess_leia.txt millenium_falcon/
 # 7. Move the millenium_falcon into the death_star.
-mv star_wars/rebellion/millenium_falcon star_wars/empire/death_star
+mv millenium_falcon/ ../empire/death_star/
--- act3.sh
+++ act3.sh
@@ -1,17 +1,20 @@
 # 1. Unload the Millenium Falcon in ONE COMMAND!
 # Move the whole crew from the millenium_falcon directory into the death_star directory.
-mv star_wars/empire/death_star/millenium_falcon/* star_wars/empire/death_star
+mv millenium_falcon/* ../death_star/
 # 2. darth_vader has defeated obi_wan! Delete poor obi_wan.
-rm star_wars/empire/death_star/obi_wan
+rm obi_wan.txt
 # 3.  Our heroes have disabled the tractor beam! In four commands, Move the whole crew back into the millenium_falcon!
 # Remember: darth_vader remains in the death_star and emperor_palpatine is still in the empire.
-mv star_wars/empire/death_star/han_solo star_wars/empire/death_star/millenium_falcon
-mv star_wars/empire/death_star/chewy star_wars/empire/death_star/millenium_falcon
-mv star_wars/empire/death_star/luke_skywalker star_wars/empire/death_star/millenium_falcon
-mv star_wars/empire/death_star/princess_leia star_wars/empire/death_star/millenium_falcon
+mv chewy.txt millenium_falcon/
+mv han_solo.txt millenium_falcon/
+mv luke_skywalker.txt millenium_falcon/
+mv princess_leia.txt millenium_falcon/
+
+
 # 4. Move the millenium_falcon back into the rebellion directory.
-mv star_wars/empire/death_star/millenium_falcon star_wars/rebellion/
+mv millenium_falcon/ ../../rebellion/
+
 # 5. darth_vader leaves the death_star to pursue Luke! Move him from the death_star into the empire directory!
-mv star_wars/empire/death_star/darth_vader star_wars/empire
+mv darth_vader.txt  ../
 # 6. Thanks to his practice back home at Beggar’s Canyon, Luke blew up the death_star! Remove it from the galaxy!
-rmdir star_wars/empire/death_star
+rm -r death_star/
